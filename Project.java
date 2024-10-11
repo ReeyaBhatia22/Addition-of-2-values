3d53fad204b6af58d70416cee4252dc5acdb6c69
ReeyaBhatia22 <reeyabhatia22@gmail.com>
2024-10-11 13:09:34 +0530

    Addition

---
+++ Project.java
@@ -0,0 +1,10 @@
+class project 
+{
+    public static void main(String[]args)
+    {
+        int a = 10;
+        int b = 12;
+        int sum= a+b; 
+        System.out.println("The sum of a and b is:"+ sum);
+    }
+}
